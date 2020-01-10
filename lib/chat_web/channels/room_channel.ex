22a11a2a2ab93d24653fdee18cbfc9281c6abb22
Brabec <lucasbbs@dcomp.ufs.br>
2020-01-10 01:17:49 -0300

    Tarefa concluída - Chat funcional

--- lib/chat_web/channels/room_channel.ex
+++ lib/chat_web/channels/room_channel.ex
@@ -3,6 +3,7 @@ defmodule ChatWeb.RoomChannel do
 
   def join("room:lobby", payload, socket) do
     if authorized?(payload) do
+      send(self(), :after_join)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -18,6 +19,7 @@ defmodule ChatWeb.RoomChannel do
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (room:lobby).
   def handle_in("shout", payload, socket) do
+    Chat.Message.changeset(%Chat.Message{}, payload) |> Chat.Repo.insert
     broadcast socket, "shout", payload
     {:noreply, socket}
   end
@@ -25,5 +27,14 @@ defmodule ChatWeb.RoomChannel do
   # Add authorization logic here as required.
   defp authorized?(_payload) do
     true
+  end
+
+  def handle_info(:after_join, socket) do
+    Chat.Message.get_messages()
+    |> Enum.each(fn msg -> push(socket, "shout", %{
+        name: msg.name,
+        message: msg.message,
+      }) end)
+      {:noreply, socket} # :noreply
   end
 end
