08b48683aaa71801c6798a49c045d2ea5a4eaf2d
Brabec <lucasbbs@dcomp.ufs.br>
2020-01-15 00:51:18 -0300

    Hotfix

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Chat.MixProject do
       aliases: aliases(),
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test,
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test,
       "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
